a98645ea0d0a6693cccaa54a4add0695b0072a3d
Thayane <thays2caroline@gmail.com>
2026-04-10 19:38:53 -0300

    Meme

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string fraseresposta, memeToNervoso = "Seu filho de uma";
+
+Console.Write("Fala oque voce acha da mim?");
+fraseresposta = Console.ReadLine()!;
+memeToNervoso = 
+$"{memeToNervoso.Remove(10)}... NÃO PERA. \n to nervoso"!;
+Console.WriteLine(memeToNervoso);
+
+
